5983ddb62acf8d0f92f1abec5410b5ba86b55d89
Truong Phan Qui <quitp@tinhvan.com>
2020-03-24 21:20:52 +0700

    add userRepository ( database in mysql you can use mysql workbench use control)

--- src/main/java/com/appsdeveloperblog/app/ws/io/entity/UserEntity.java
+++ src/main/java/com/appsdeveloperblog/app/ws/io/entity/UserEntity.java
@@ -7,7 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-//@Entity(name = "users")
+@Entity(name = "users")
 public class UserEntity implements Serializable {
 
 	/**
--- src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/impl/UserServiceImpl.java
@@ -4,7 +4,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-//import com.appsdeveloperblog.app.ws.UserRepository;
+import com.appsdeveloperblog.app.ws.UserRepository;
 import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
 import com.appsdeveloperblog.app.ws.service.UserService;
 import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
@@ -12,8 +12,8 @@ import com.appsdeveloperblog.app.ws.shared.dto.UserDto;
 @Service
 public class UserServiceImpl implements UserService {
 
-//	@Autowired
-//	UserRepository UserRepository;
+	@Autowired
+	UserRepository userRepository;
 	
 	@Override
 	public UserDto createUser(UserDto user) {
@@ -22,14 +22,14 @@ public class UserServiceImpl implements UserService {
 		UserEntity userEntity = new UserEntity();
 		BeanUtils.copyProperties(user, userEntity);
 		
-		userEntity.setEmail("test@gmail.com");
+		userEntity.setEncrytedPassword("test");
 		userEntity.setUserId("testUserId");
 		
 		
-		//UserEntity storedUserDetails = UserRepository.save(userEntity);
+		UserEntity storedUserDetails = userRepository.save(userEntity);
 		
 		UserDto returnValue = new UserDto();
-		BeanUtils.copyProperties(userEntity, returnValue);
+		BeanUtils.copyProperties(storedUserDetails, returnValue);
 		
 		return returnValue;
 	}
