d843c4cf2a576d652dd13929616bbc281a6d410f
Truong Phan Qui <quitp@tinhvan.com>
2020-03-24 14:23:09 +0700

    khong chac lam

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -1,8 +1,13 @@
 package com.appsdeveloperblog.app.ws.ui.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.appsdeveloperblog.app.ws.ui.model.request.UserDetailsRequestModel;
+import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
 
 @RestController
 @RequestMapping("users")
@@ -13,5 +18,9 @@ public class UserController {
 		return "get user was call";
 	}
 	
+	@PostMapping
+	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
+		return null;
+	}
 	
 }
